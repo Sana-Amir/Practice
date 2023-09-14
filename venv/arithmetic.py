2784846c0cc4fc9c7f70357e55a8e52a51d20864
Taimoor Amir <taimooramir0079@gmail.com>
2023-09-14 20:12:29 +0200

    Commit

---
+++ venv/arithmetic.py
@@ -0,0 +1,13 @@
+x = 6
+y = 5
+z = x + y
+j = x - y
+m =  x * y
+d =  x / y
+r = x % y
+
+print(str(x) + "+" +  str(y )+ "=" + str(z))
+print(str(x) + "-" +  str(y )+ "=" + str(j))
+print(str(x) + "*" +  str(y )+ "=" + str(m))
+print(str(x) + "/" +  str(y )+ "=" + str(d))
+print(str(x) + "%" +  str(y )+ "=" + str(r))
