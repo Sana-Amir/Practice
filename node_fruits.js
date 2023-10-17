5d76ced0a0e4353bfb346e76f15d3bfaee796c26
Taimoor Amir <sanaamir4274@gmail.com>
2023-10-17 09:49:51 +0200

    COMMIT

---
+++ node_fruits.js
@@ -0,0 +1,12 @@
+let fruits = prompt("enter your fruit name");
+switch (fruits){
+case "mango":
+    case "peach":
+        console.log("its mango field");
+        break;
+    case "apple":
+        console.log("its apple field");
+        break;
+    default:
+        console.log("its wrong value")
+}
